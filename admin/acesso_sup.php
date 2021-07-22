7be23916f0068e2b86f7d053746277ad1c4e4aa0
Iwanezuk <41554607+Iwanezuk@users.noreply.github.com>
2021-07-22 20:20:08 -0300

    Create acesso_sup.php

---
+++ admin/acesso_sup.php
@@ -0,0 +1,43 @@
+<?php
+// Salvar como: admin/acesso_com.php
+// A sessão precisa ser iniciada em cada página diferente
+// Se a sessão não existir, iniciar uma
+// Determinar o nível de acesso se necessário
+// Inicia a sessão
+session_name("chulettaaa");
+
+if(!isset($_SESSION)){
+    session_start();
+};
+
+// verificar se o usuário está logado na sessão
+// identifica o usuario
+if(!isset($_SESSION['login_usuario'])){
+    // Se não existir redirecionamos a sessão por segurança
+    header("Location: login.php"); exit;
+};
+
+$nome_da_sessao =   session_name();
+
+// verificar o nome da sessão
+if(!isset($_SESSION['nome_da_sessao']) OR ($_SESSION['nome_da_sessao']!=$nome_da_sessao)){
+    // Se não existir, destruimos a sessão por segurança
+    session_destroy();
+    header("Location: login.php"); exit;
+};
+
+// Determinar o nível de acesso;
+$nivel_acesso   =   'sup';
+if(!isset($_SESSION['login_usuario']) OR ($_SESSION['nivel_usuario']!=$nivel_acesso)){
+    // Redireciona para página de autorização
+    header("Location: invasor_user.php"); exit;
+};
+
+
+// verificar se o login é válido
+if(!isset($_SESSION['login_usuario'])){
+    // Se não existir, destruimos a sessão por segurança
+    session_destroy();
+    header("Location: login.php"); exit;
+};
+?>
